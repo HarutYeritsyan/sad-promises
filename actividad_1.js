eab34dae07c52d26c992e4fd653ea37f09536790
Harut <harutyun.yer@gmail.com>
2019-01-05 14:19:17 +0100

    Added asynchronous callback invocation to retrieve function

--- actividad_1.js
+++ actividad_1.js
@@ -1,20 +1,30 @@
-var fs = require('fs');
+// synchronous loading
+var rolodexFile = {
+    a: 'We know A',
+    b: 'We know B',
+    c: 'We know C'
+};
 
-var rolodexFile = fs.open('My rollodex file');
 var rolodex = { a: 'We know this name' };
 
 function retrieve(file, name, cb) {
 
     //	Searches for name in file, and
-    //	invokes cb with record found
+    //	invokes cb asynchronously
+    setTimeout(function () {
+        var person = file[name];
+        cb(person);
+    }, 0);
 }
 
 function processEntry(name, cb) {
 
     if (rolodex[name]) {
+        // callback is called synchronously
         cb(rolodex[name]);
     }
     else {
+        // callback function will be called asynchronously by retrieve()
         retrieve(rolodexFile, name, function (val) {
             rolodex[name] = val;
             cb(val);
