bea35e0fc2e35c33d30a6b0f93ac7c7c0e57e186
Harut <harutyun.yer@gmail.com>
2019-01-06 21:52:44 +0100

    Refactored https_server

--- https_server.js
+++ https_server.js
@@ -5,6 +5,7 @@ var itemList = [];
 var updateItemList = function () {
     return cart.getItems().then(function (items) {
         itemList = items;
+        console.log('items: ', itemList);
     }).catch(function (err) {
         console.log('Could not retrieve item list from shopping cart: ', err);
     });
@@ -12,13 +13,16 @@ var updateItemList = function () {
 
 cart.initialize().then(function () {
     updateItemList().then(function () {
-        console.log('initial items: ', itemList);
-        cart.addItemToCart({ cod: 999, desc: 'prueba-item' }).then(function () {
-            updateItemList().then(console.log('updated items: ', itemList));
-        }).catch(function (err) {
-            console.log('Could not add item to cart: ', err);
-        });
+        testCart();
     });
 }).catch(function (err) {
     console.log('Could not initialize cart: ', err);
 });
+
+var testCart = function () {
+    cart.addItemToCart({ cod: 999, desc: 'prueba-item' }).then(function () {
+        updateItemList();
+    }).catch(function (err) {
+        console.log('Could not add item to cart: ', err);
+    });
+}
