49139204607a5a12817e5ce3d1c6894810244930
Harut <harutyun.yer@gmail.com>
2019-01-06 20:56:43 +0100

    Added https_server for using cart module

---
+++ https_server.js
@@ -0,0 +1,20 @@
+var cart = require('./carrito.js');
+
+var itemList = [];
+
+var updateItemList = function () {
+    return cart.getItems().then(function (items) {
+        itemList = items;
+    }).catch(function (err) {
+        console.log('Could not retrieve item list from shopping cart: ', err);
+    });
+}
+
+updateItemList().then(function () {
+    console.log('initial items: ', itemList);
+    cart.addItemToCart({ cod: 999, desc: 'prueba-item' }).then(function () {
+        updateItemList().then(console.log('updated items: ', itemList));
+    }).catch(function (err) {
+        console.log('Could not add item to cart: ', err);
+    })
+});
