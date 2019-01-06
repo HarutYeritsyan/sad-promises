463e503db30bbde2b0b724f1778542329cd92ed6
Harut <harutyun.yer@gmail.com>
2019-01-06 18:01:17 +0100

    [ACT-2] Added collection listing function to mongodb client

--- Cliente_mongo.js
+++ Cliente_mongo.js
@@ -4,23 +4,30 @@ var assert = require('assert');
 
 var mongoclient = mgdb.MongoClient;
 
-var url = 'mongodb://localhost:8100/almacen'
+var url = 'mongodb://test:abc123@ds141368.mlab.com:41368/sad-promises'
 
-mongoclient.connect(url, function (err, db) {
+mongoclient.connect(url, function (err, client) {
   assert.equal(err, null);
   console.log('conectado');
 
-	/*db.createCollection("documents", function(err, res) {
-    	if (err) throw err;
-    	console.log("Collection created!");
-  	});*/
-  insertDocuments(db, function () {
+  var db = client.db('sad-promises');
+
+  /*
+  db.createCollection("documents", function (err, res) {
     if (err) throw err;
-    console.log('success with insertion!');
-    db.close();
+    console.log("Collection created!");
+    insertDocuments(db, function () {
+      if (err) throw err;
+      console.log('success with insertion!');
+      client.close();
+    });
   });
+  */
 
-
+  getDocuments(db, function (documents) {
+    console.log(documents);
+    client.close();
+  });
 });
 
 
@@ -37,5 +44,13 @@ var insertDocuments = function (db, callback) {
     assert.equal(3, result.ops.length);
     console.log("Inserted 3 products into the collection");
     callback(result);
+  });
+}
+
+var getDocuments = function (db, callback) {
+  var collection = db.collection('products');
+  collection.find({ stock: { $gte: 5 } }).toArray(function (err, docs) {
+    if (err) throw err;
+    callback(docs);
   });
 }
