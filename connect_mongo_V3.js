7c993945e90a1a525263199892d0800bd0fc486c
Harut <harutyun.yer@gmail.com>
2019-01-09 18:44:39 +0100

    Format

--- connect_mongo_V3.js
+++ connect_mongo_V3.js
@@ -6,25 +6,25 @@ var mongoClient = mgdb.MongoClient;
 var connection;
 
 exports.connect = function (url) {
-    return new Promise(function (resolve, reject) {
-  
-      mongoClient.connect(url, function (err, client) {
-        if (err) reject(err);
-        assert.equal(err, null);
-  
-        console.log('connected');
-        connection = client;
-        db = client.db('sad-promises');
-        resolve(db);
-      });
+  return new Promise(function (resolve, reject) {
+
+    mongoClient.connect(url, function (err, client) {
+      if (err) reject(err);
+      assert.equal(err, null);
+
+      console.log('connected');
+      connection = client;
+      db = client.db('sad-promises');
+      resolve(db);
     });
-  }
-  
-  exports.disconnect = function () {
-    return new Promise(function (resolve) {
-      if (connection) {
-        connection.close();
-      }
-      resolve();
-    });
-  }
+  });
+}
+
+exports.disconnect = function () {
+  return new Promise(function (resolve) {
+    if (connection) {
+      connection.close();
+    }
+    resolve();
+  });
+}
